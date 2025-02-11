0883323d4aa7837d1210860cd262336741196d7e
Nikita <tyumin000@gmail.com>
2025-02-11 17:34:10 +0300

    fix: fix migrator

--- backend/cmd/migrator/migrator.go
+++ backend/cmd/migrator/migrator.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"markup/internal/config"
 )
 
