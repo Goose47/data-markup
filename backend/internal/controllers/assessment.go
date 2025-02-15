edfbc9fc733220c059766795d6d8ab2394622395
Nikita <tyumin000@gmail.com>
2025-02-15 17:00:11 +0300

    fix: fix sql syntax

--- backend/internal/controllers/assessment.go
+++ backend/internal/controllers/assessment.go
@@ -217,12 +217,12 @@ func (con *Assessment) Next(c *gin.Context) {
 	}
 	err = con.db.
 		Table("markups").
-		Select("markups.id, COUNT(assessments.id) as assessments_count, batches.overlaps, markups.status_id").
+		Select("markups.id, COUNT(assessments.id), batches.overlaps, markups.status_id").
 		Where("status_id = ?", markupStatus.Pending).
 		Joins("JOIN batches ON markups.batch_id = batches.id").
 		Joins("LEFT JOIN assessments ON assessments.markup_id = markups.id").
 		Group("markups.id").
-		Having("assessments_count < batches.overlaps").
+		Having("COUNT(assessments.id) < batches.overlaps").
 		First(&res).Error
 
 	if err != nil {
