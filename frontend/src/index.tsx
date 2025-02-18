12926669d55ee8f945300eefeb4c3c1d9ceddda8
artem <uvuv.space@gmail.com>
2025-02-18 18:01:42 +0300

    feat: strict mode disabled

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,9 +11,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme="dark">
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme="dark">
+    <App />
+  </ThemeProvider>
 );
